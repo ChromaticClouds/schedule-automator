3e6e59ce0288bfa3c7a61222fa67df2c0d1b363a
ChromaticClouds <kddo3339@gmail.com>
2026-07-05 08:45:52 +0900

    fix(mobile): isolate storybook entry

--- ai-scheduler-mobile/metro.config.js
+++ ai-scheduler-mobile/metro.config.js
@@ -1,6 +1,8 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const { withStorybook } = require('@storybook/react-native/withStorybook');
+const { withStorybook } = require('@storybook/react-native/metro/withStorybook');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withStorybook(config);
+module.exports = withStorybook(config, {
+  enabled: process.env.STORYBOOK_ENABLED === 'true',
+});
